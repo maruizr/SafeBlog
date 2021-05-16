483d76ab610fc3dd51ceaa50540c85f1656203a8
cesar <ces.jofre@duocuc.cl>
2021-05-16 00:01:55 -0700

    Se anade el activity comment

---
+++ app/src/main/java/com/example/appblog/PostDetailActivity.java
@@ -0,0 +1,343 @@
+package com.example.appblog;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.ClipData;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class PostDetailActivity extends AppCompatActivity {
+
+
+    //para tener los detalles del usuraio y el post
+    String myUid, MyEmail, myName, myDp,
+    postId, pLikes, hisDp, hisName;
+
+    boolean mProcessComment = false;
+    boolean mProcessLike = false;
+
+    //barra de progreso
+    ProgressDialog pd;
+
+    //views
+    ImageView uPictureIv, pImageIv;
+    TextView uNameTv, pTimeTiv, pTitleTv, pDescriptionTv, plikesTv, pCommentsTv;
+    ImageButton moreBtn;
+    Button likeBtn, shareBtn;
+    LinearLayout profileLayout;
+
+    //views comentarios
+    EditText commentEt;
+    ImageButton sendBtn;
+    ImageView cAvatarIv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_detail);
+
+        //barra y sus propiedades
+        ActionBar actionBar = getSupportActionBar();
+
+
+        //tener id del post usando itent
+        Intent intent = getIntent();
+        postId = intent.getStringExtra("postId");
+
+        //init views
+        uPictureIv = findViewById(R.id.uPictureIv);
+        pImageIv = findViewById(R.id.imgIv);
+        uNameTv = findViewById(R.id.uNameTv);
+        pTimeTiv = findViewById(R.id.timeTv);
+        pTitleTv = findViewById(R.id.titleTv);
+        pDescriptionTv = findViewById(R.id.descTv);
+        plikesTv = findViewById(R.id.likesTv);
+        pCommentsTv = findViewById(R.id.pCommentsTv);
+        moreBtn = findViewById(R.id.moreBtn);
+        likeBtn = findViewById(R.id.likeBtn);
+        shareBtn = findViewById(R.id.shareBtn);
+        profileLayout = findViewById(R.id.profileLayout);
+
+        commentEt = findViewById(R.id.commentEt);
+        sendBtn = findViewById(R.id.sendBtn);
+        cAvatarIv = findViewById(R.id.avatarIv);
+
+        loadPostInfo();
+
+        checkUserStatus();
+
+        loadUserInfo();
+
+        assert actionBar != null;
+        actionBar.setSubtitle("Has ingresado como: "+MyEmail);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postComment();
+            }
+        });
+
+
+        likeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                likePost();
+            }
+        });
+    }
+
+    private void likePost() {
+
+        mProcessLike = true;
+
+        DatabaseReference likesRef = FirebaseDatabase.getInstance().getReference().child("Likes");
+        DatabaseReference postsRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+        likesRef.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(mProcessLike){
+                    if(snapshot.child(postId).hasChild(myUid)){
+                        postsRef.child(postId).child("pLikes").setValue(""+(Integer.parseInt(pLikes)-1));
+                        likesRef.child(postId).child(myUid).removeValue();
+                        mProcessLike = false;
+
+                        likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_like_black,0,0,0);
+                        likeBtn.setText("Like");
+                    }
+                    else{
+                        postsRef.child(postId).child("pLikes").setValue(""+(Integer.parseInt(pLikes)-1));
+                        likesRef.child(postId).child(myUid).setValue("Liked");
+                        mProcessLike = false;
+
+                        likeBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_liked,0,0,0);
+                        likeBtn.setText("Liked");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void postComment() {
+        pd = new ProgressDialog(this);
+        pd.setMessage("Agregar un comentario...");
+
+        //obeter datos del comentario editado
+        String comment = commentEt.getText().toString().trim();
+        //validar
+        if (TextUtils.isEmpty(comment)){
+            Toast.makeText(this,"Comentario vacio...",Toast.LENGTH_LONG).show();
+            return;
+        }
+        String timeStamp = String.valueOf(System.currentTimeMillis());
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts").child(postId).child("Comments");
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("cId", timeStamp);
+        hashMap.put("comment", comment);
+        hashMap.put("pComments", "0");
+        hashMap.put("timestamp", timeStamp);
+        hashMap.put("uid", myUid);
+        hashMap.put("uEmail", MyEmail);
+        hashMap.put("uDp", myDp);
+        hashMap.put("uName", myName);
+
+        ref.child(timeStamp).setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid){
+                        pd.dismiss();
+                        Toast.makeText(PostDetailActivity.this,"Comentario realizado...", Toast.LENGTH_SHORT).show();
+                        commentEt.setText("");
+                        updateCommentCount();
+                    }
+
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(PostDetailActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+    private void updateCommentCount() {
+        mProcessComment = true;
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts").child(postId);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (mProcessComment){
+                    String comments = "" + dataSnapshot.child("pComments").getValue();
+                    int newCommentVal = Integer.parseInt(comments) + 1;
+                    ref.child("pComments").setValue(""+newCommentVal);
+                    mProcessComment = false;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull  DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void loadUserInfo() {
+        //info actual del usuario
+        Query myRef = FirebaseDatabase.getInstance().getReference("Users");
+        myRef.orderByChild("uid").equalTo(myUid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+              for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                  myName = ""+ds.child("name").getValue();
+                  myDp = ""+ds.child("image").getValue();
+
+
+              }
+            }
+
+            @Override
+            public void onCancelled(@NonNull  DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void loadPostInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+        Query query = ref.orderByChild("pId").equalTo(postId);
+        query.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    String pTitle = ""+ds.child("pTitle").getValue();
+                    String pDescr= ""+ds.child("pDescr").getValue();
+                    pLikes  = ""+ds.child("pLikes").getValue();
+                    String pTimeStamp= ""+ds.child("pTime").getValue();
+                    String pImage= ""+ds.child("pImage").getValue();
+                    hisDp = ""+ds.child("uDp").getValue();
+                    String uid= ""+ds.child("uid").getValue();
+                    String uEmail= ""+ds.child("uEmail").getValue();
+                    hisName = ""+ds.child("uName").getValue();
+                    String commentCount = ""+ds.child("pComments").getValue();
+
+                    Calendar calendar = Calendar.getInstance(Locale.getDefault());
+                    calendar.setTimeInMillis(Long.parseLong(pTimeStamp));
+                    String pTime = DateFormat.format("dd/MM/yyyy hh:mm aa",calendar).toString();
+
+
+                    pTitleTv.setText(pTime);
+                    pDescriptionTv.setText(pDescr);
+                    plikesTv.setText(pLikes + "Likes");
+                    pTimeTiv.setText(pTime);
+                    pCommentsTv.setText(commentCount +" Comentarios");
+
+                    uNameTv.setText(hisName);
+
+                    //coloca la imagen del usuario que posteo
+                    if(pImage.equals("noImage")){
+                        pImageIv.setVisibility(View.GONE);
+                    }
+                    else{
+                        pImageIv.setVisibility(View.VISIBLE);
+                        try{
+                            Picasso.get().load(pImage).into(pImageIv);
+                        }
+                        catch (Exception e){
+
+                        }
+                    }
+
+                    try{
+                        Picasso.get().load(hisDp).placeholder(R.drawable.ic_default_img).into(uPictureIv);
+                    }
+                    catch (Exception e) {
+                        Picasso.get().load(R.drawable.ic_default_img).into(uPictureIv);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void checkUserStatus(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!= null){
+            MyEmail = user.getEmail();
+            myUid = user.getUid();
+        }
+        else {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        menu.findItem(R.id.action_add_post).setVisible(false);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.actionLogOut){
+            FirebaseAuth.getInstance().signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
